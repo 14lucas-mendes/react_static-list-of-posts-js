04e128a761c4b9499d0bb5617eb24aeb4563b5ff
lucas mendes <luckajw@gmail.com>
2025-02-18 21:16:16 -0300

    postlist renderizado e criado e estabelecido herança com postinfo

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,1 +1,21 @@
-export const PostInfo = () => <>Put the post here</>;
+export const PostInfo = ({ posts, user }) => (
+  <div className="PostInfo">
+    <div className="PostInfo__header">
+      <h3 className="PostInfo__title">{posts.title}</h3>
+
+      <p>
+        {' Posted by  '}
+
+        <a className="UserInfo" href={user.email}>
+          {user.name}
+        </a>
+      </p>
+    </div>
+
+    <p className="PostInfo__body">{posts.body}</p>
+
+    <hr />
+
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  </div>
+);
